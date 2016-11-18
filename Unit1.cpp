cd1b3f95282e180e7af3ccf2bcbfafddd95572d8
Mayorov Maxim <nub.pun@yandex.ru>
2016-11-18 14:20:58 +0500

    complete blending

---
+++ Unit1.cpp
@@ -0,0 +1,267 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <cmath>
+#include "Unit1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+Camera cam(5, 45, 45);
+
+BOOL TForm1::bSetupPixelFormat(HDC hDC)
+
+{
+
+  PIXELFORMATDESCRIPTOR pfd; //Создаем структуру
+
+  int pixelformat;
+
+  pfd.nSize = sizeof (PIXELFORMATDESCRIPTOR); //Размер структуры
+
+  pfd.nVersion = 1;  //Версия структуры
+
+  pfd.dwFlags = PFD_DOUBLEBUFFER | PFD_SUPPORT_OPENGL | PFD_DRAW_TO_WINDOW;
+
+  pfd.iLayerType =  PFD_MAIN_PLANE;  //Тип поверхности
+
+  pfd.iPixelType = PFD_TYPE_RGBA;  //Формат указания цвета
+
+  pfd.cColorBits = 16;  //Глубина цвета
+
+  pfd.cDepthBits = 16;  //Размер буфера глубины
+
+  pfd.cAccumBits = 0; //Общее число битовых плоскостей в буфере аккумулятора
+
+  pfd.cStencilBits = 0;  //Размер буфера трафарета
+
+  if (!(pixelformat=ChoosePixelFormat(hDC, &pfd))) {
+
+    MessageBox(NULL, "Невозможно выбрать формат пикселей", "Error", MB_OK);
+
+    return false;
+
+  }
+
+  if (!SetPixelFormat (hDC, pixelformat, &pfd)) {
+
+    MessageBox(NULL, "Невозможно установить формат пикселей", "Error", MB_OK);
+
+    return false;
+
+  }
+
+  return true;
+
+}
+
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::FormCreate(TObject *Sender)
+{
+hDC = GetDC(Handle); //Handle – дескриптор окна (hwnd в WinAPI)
+if (!bSetupPixelFormat(hDC)) //Устанавливаем формат пикселей
+  return;
+ghRC = wglCreateContext(hDC); //Создаем контекст воспроизведения
+wglMakeCurrent(hDC, ghRC);  //Делаем его текущим
+glClearColor(0.1, 0.1, 0.1, 0.0); //Цвет экрана при очищении
+glEnable(GL_COLOR_MATERIAL);  //Разрешаем задавать цвет объектам
+glEnable(GL_DEPTH_TEST);  //Тест глубины для объемности изображения
+glEnable(GL_LIGHTING);  //Разрешаем освещение
+glEnable(GL_LIGHT0);  //Включили освещение 0
+	glEnable(GL_ALPHA_TEST);
+	glEnable(GL_BLEND); glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+float p[4]={100,100,100,5};
+glLightfv(GL_LIGHT0,GL_POSITION,p);  //Установка позиции освещения
+
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::FormDestroy(TObject *Sender)
+{
+	if(ghRC)
+	{
+		wglMakeCurrent(hDC,0);
+		wglDeleteContext(ghRC);
+	}
+	if(hDC) ReleaseDC(Handle, hDC);
+}
+//---------------------------------------------------------------------------
+  void __fastcall TForm1::FormResize(TObject *Sender)
+{
+	 moveCamera();
+}
+//---------------------------------------------------------------------------
+using namespace std;
+     void DrawFish()
+{
+
+	glBegin(GL_QUADS);                      // Рисуем куб
+		glColor3f(1.0f,1.0f,1.0f);              // Зеленый
+		glVertex3f( 0.5f, 0.5f,-0.5f);          // Право верх квадрата (Верх)
+		glVertex3f(-0.5f, 0.5f,-0.5f);          // Лево верх
+		glVertex3f(-0.5f, 0.5f, 0.5f);          // Лево низ
+		glVertex3f( 0.5f, 0.5f, 0.5f);          // Право низ
+
+		glColor3f(1.0f,0.5f,0.0f);              // Оранжевый
+		glVertex3f( 0.5f,-0.5f, 0.5f);          // Верх право квадрата (Низ)
+		glVertex3f(-0.5f,-0.5f, 0.5f);          // Верх лево
+		glVertex3f(-0.5f,-1.0f,-0.5f);          // Низ лево
+		glVertex3f( 0.5f,-0.5f,-0.5f);          // Низ право
+
+        glColor3f(1.0f,0.0f,0.0f);              // Красный
+		glVertex3f( 0.5f, 0.5f, 0.5f);          // Верх право квадрата (Перед)
+		glVertex3f(-0.5f, 0.5f, 0.5f);          // Верх лево
+		glVertex3f(-0.5f,-0.5f, 0.5f);          // Низ лево
+		glVertex3f( 0.5f,-0.5f, 0.5f);          // Низ право
+
+		glColor3f(1.0f,1.0f,0.0f);              // Желтый
+		glVertex3f( 0.5f,-0.5f,-0.5f);          // Верх право квадрата (Зад)
+		glVertex3f(-0.5f,-0.5f,-0.5f);          // Верх лево
+		glVertex3f(-0.5f, 0.5f,-0.5f);          // Низ лево
+		glVertex3f( 0.5f, 0.5f,-0.5f);          // Низ право
+
+		glColor3f(0.0f,0.0f,1.0f);              // Синий
+		glVertex3f(-0.5f, 0.5f, 0.5f);          // Верх право квадрата (Лево)
+		glVertex3f(-0.5f, 0.5f,-0.5f);          // Верх лево
+		glVertex3f(-0.5f,-0.5f,-0.5f);          // Низ лево
+		glVertex3f(-0.5f,-0.5f, 0.5f);          // Низ право
+
+		glColor3f(1.0f,0.0f,1.0f);              // Фиолетовый
+		glVertex3f( 0.5f, 0.5f,-0.5f);          // Верх право квадрата (Право)
+		glVertex3f( 0.5f, 0.5f, 0.5f);          // Верх лево
+		glVertex3f( 0.5f,-0.5f, 0.5f);          // Низ лево
+		glVertex3f( 0.5f,-0.5f,-0.5f);          // Низ право
+	glEnd();
+}
+void DrawAquarium()
+{
+
+	glBegin(GL_QUADS);                      // Рисуем куб
+		glColor4f(1.0f,1.0f,1.0f, 0.1);              // Зеленый
+        glVertex3f( 1.0f, 1.0f,-1.0f);          // Право верх квадрата (Верх)
+        glVertex3f(-1.0f, 1.0f,-1.0f);          // Лево верх
+        glVertex3f(-1.0f, 1.0f, 1.0f);          // Лево низ
+		glVertex3f( 1.0f, 1.0f, 1.0f);          // Право низ
+
+        glColor3f(1.0f,0.5f,0.0f);              // Оранжевый
+        glVertex3f( 1.0f,-1.0f, 1.0f);          // Верх право квадрата (Низ)
+        glVertex3f(-1.0f,-1.0f, 1.0f);          // Верх лево
+        glVertex3f(-1.0f,-1.0f,-1.0f);          // Низ лево
+		glVertex3f( 1.0f,-1.0f,-1.0f);          // Низ право
+
+        glColor3f(1.0f,0.0f,0.0f);              // Красный
+        glVertex3f( 1.0f, 1.0f, 1.0f);          // Верх право квадрата (Перед)
+        glVertex3f(-1.0f, 1.0f, 1.0f);          // Верх лево
+        glVertex3f(-1.0f,-1.0f, 1.0f);          // Низ лево
+		glVertex3f( 1.0f,-1.0f, 1.0f);          // Низ право
+
+		glColor3f(1.0f,1.0f,0.0f);              // Желтый
+        glVertex3f( 1.0f,-1.0f,-1.0f);          // Верх право квадрата (Зад)
+        glVertex3f(-1.0f,-1.0f,-1.0f);          // Верх лево
+        glVertex3f(-1.0f, 1.0f,-1.0f);          // Низ лево
+		glVertex3f( 1.0f, 1.0f,-1.0f);          // Низ право
+
+		glColor3f(0.0f,0.0f,1.0f);              // Синий
+        glVertex3f(-1.0f, 1.0f, 1.0f);          // Верх право квадрата (Лево)
+        glVertex3f(-1.0f, 1.0f,-1.0f);          // Верх лево
+        glVertex3f(-1.0f,-1.0f,-1.0f);          // Низ лево
+		glVertex3f(-1.0f,-1.0f, 1.0f);          // Низ право
+
+		glColor3f(1.0f,0.0f,1.0f);              // Фиолетовый
+        glVertex3f( 1.0f, 1.0f,-1.0f);          // Верх право квадрата (Право)
+        glVertex3f( 1.0f, 1.0f, 1.0f);          // Верх лево
+        glVertex3f( 1.0f,-1.0f, 1.0f);          // Низ лево
+        glVertex3f( 1.0f,-1.0f,-1.0f);          // Низ право
+	glEnd();
+}
+void TForm1::Draw()
+{
+	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+	DrawFish();
+	DrawAquarium();
+	SwapBuffers(hDC);
+}
+
+
+
+
+void __fastcall TForm1::FormPaint(TObject *Sender)
+{
+	Draw();
+}
+//---------------------------------------------------------------------------
+
+void TForm1::moveCamera(){
+  double x, y, dx, dy;
+
+  x = Form1->Left;
+  y = Form1->Top;
+  dx = Form1->Width;
+  dy = Form1->Height;
+  glViewport(x, y, dx, dy);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(70.0, dx/dy, 0.1f, 200.f);
+  point eye = cam.getEye();
+  point sky = cam.getNorm();
+  gluLookAt(eye.x, eye.y, eye.z, 0, 0, 0, sky.x, sky.y, sky.z);
+  glMatrixMode(GL_MODELVIEW);
+}
+
+void __fastcall TForm1::FormKeyDown(TObject *Sender, WORD &Key,
+      TShiftState Shift)
+{
+	switch(Key){
+		case(VK_RIGHT):{
+            cam.rotate(0, -1);
+            moveCamera();
+            Draw();
+			break;
+		}
+		case(VK_LEFT):{
+            cam.rotate(0, 1);
+            moveCamera();
+            Draw();
+			break;
+		}
+		case(VK_DOWN):{
+            cam.rotate(1, 0);
+            moveCamera();
+            Draw();
+			break;
+		}
+		case(VK_UP):{
+            cam.rotate(-1, 0);
+            moveCamera();
+            Draw();
+			break;
+		}
+		case(VK_ADD):
+        case(VK_OEM_PLUS):{
+			cam.zoom(-1);
+            moveCamera();
+            Draw();
+            break;
+		}
+        case(VK_SUBTRACT ):
+        case(VK_OEM_MINUS):{
+			cam.zoom(1);
+            moveCamera();
+            Draw();
+			break;
+		}
+        default:
+            break;
+   }
+}
+//---------------------------------------------------------------------------
+
---
+++ Unit1.h
@@ -0,0 +1,40 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+
+#include <ExtCtrls.hpp>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include "Camera.h"
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	void __fastcall FormCreate(TObject *Sender);
+	void __fastcall FormDestroy(TObject *Sender);
+	void __fastcall FormResize(TObject *Sender);
+	void __fastcall FormPaint(TObject *Sender);
+	void __fastcall FormKeyDown(TObject *Sender, WORD &Key, TShiftState Shift);
+	
+private:	// User declarations
+	HGLRC ghRC;  // указатель на контекст воспроизведения (Rendering Context)
+	HDC   hDC; // дескриптор (контекст) устройства
+public:		// User declarations
+void TForm1::Scale(double sx, double sy);
+void TForm1::Rotate(double w);
+void TForm1::InitFigure();
+	void TForm1::Draw();
+	    void moveCamera();
+	__fastcall TForm1(TComponent* Owner);
+	BOOL bSetupPixelFormat(HDC hDC);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
